e055f39cecfb359e7bfc675bd7c614389d981aa8
21Maksym <maksimbukhalo1314@gmail.com>
2020-05-31 16:24:10 +0300

    Заміна версії .net на 4.0

--- GUI2/Properties/Resources.Designer.cs
+++ GUI2/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Lab3.Properties {
+namespace GUI2.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Lab3.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Lab3.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GUI2.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
